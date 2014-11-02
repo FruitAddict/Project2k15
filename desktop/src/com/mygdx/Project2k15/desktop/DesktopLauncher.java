f91b2fd26a8ff057505bb4592ca3ac0b1e30f060
FruitAddict <shadowyappendage@gmail.com>
2014-11-02 19:06:34 +0100

    Initial gdx template

---
+++ desktop/src/com/mygdx/Project2k15/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.Project2k15.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.Project2k15.Project2k15;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Project2k15(), config);
+	}
+}

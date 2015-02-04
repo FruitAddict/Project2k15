6cdbe0405f7688a7c7686f934db7eaa179c1919e
FruitAddict <shadowyappendage@gmail.com>
2015-02-04 22:02:30 +0100

    Reset

--- core/src/com/fruit/game/visual/tween/TweenUtils.java
+++
@@ -1,7 +0,0 @@
-package com.fruit.game.visual.tween;
-
-import aurelienribon.tweenengine.TweenManager;
-
-public class TweenUtils {
-    public static TweenManager tweenManager = new TweenManager();
-}
